4690beff1659c76eabeb0716372ffe3263433be8
isira <it19017334@my.sliit.lk>
2022-05-11 19:16:44 +0530

    init

---
+++ BACKEND/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose"); 
+const bodyParser = require("body-parser"); 
+const cors = require("cors"); 
+const dotenv = require("dotenv"); 
+const app = express(); 
+dotenv.config();
+
+const PORT = process.env.PORT || 8070;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const URL = process.env.MONGODB_URL;
+
+
+mongoose.connect(URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("Mongodb conncetion success!");
+});
+
+app.listen(PORT, () => {
+    console.log('Server is up and running on port: ' + PORT);
+})
